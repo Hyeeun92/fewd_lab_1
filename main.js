2a061ff552e180cbf733d506ef8e0dde854a24eb
Hyeeun92 <21545@student.dorset-college.ie>
2020-12-14 13:40:21 +0000

    commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var firstName = 'Hyeeun Lee';
+var favoriteColor = 'blue';
+var favouriteFood = 'pizza';
+var city = 'Lubumbashi';
+var county = 'Congo';
+// show the first name in console
+console.log(firstName);
+// show the favorite food in console
+console.log(favoriteColor);
+// show the favorite food in console
+console.log(favouriteFood);
+// show the city in console
+console.log(city);
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+console.log(phrase+' '+one+', '+two+', '+three+', '+four+', '+five+', '+six+', '+seven+' '+endingPhrase);
